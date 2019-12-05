a20843f7eb3a7091478179a5e42ce75422b78437
Jithin Balan <jithin.balan@outware.com.au>
2019-12-05 15:11:22 +1100

    Text 1

--- Mystash/Mystash/ContentView.swift
+++ Mystash/Mystash/ContentView.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
-        print("# test")
-        print("4 commit")
+        Text("1 text")
     }
 }
 
