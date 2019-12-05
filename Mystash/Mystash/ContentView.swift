194367fd36940d4ea87bc2c05460645099ae41ed
Jithin Balan <jithin.balan@outware.com.au>
2019-12-05 15:03:35 +1100

    Initial commit

---
+++ Mystash/Mystash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mystash
+//
+//  Created by Jithin Balan on 5/12/19.
+//  Copyright © 2019 Jithin Balan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
